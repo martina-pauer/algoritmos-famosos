de4aa48b8610cbe1baadb4ab87377430480c5b43
Martina Pauer <martinapauer38@gmail.com>
2025-04-10 21:39:31 +0000

    Primer ejemplo con implementación de paridad de turing

---
+++ paridad-de-turing/paridad.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+def paridad_turing(digitos : list) -> list:
+    '''
+        Cuenta cuantos digitos '1' hay en la lista de digitos,
+        si es una cantidad par agrega digito '0' como último digito
+        sino agrega 1
+    '''
+    if ((digitos.count(1) % 2) == 0):
+        # Agrego cero ante paridad de unos para equilibrar
+        digitos.append(0)
+    else:
+        # Agrego uno ante la paridad de ceros para evitar desproporcion
+        digitos.append(1)
+
+    return digitos
+
+def corregir(digitos : list) -> str:
+    '''
+        Reconvierte digitos en un texto para salida
+        mas bonita
+    '''
+    texto = ''
+
+    for digito in digitos:
+        texto += digito.__str__()
+        
+    return texto    
+
+# Uso un bucle que recorre listas bidimensionales para hacer demostración
+print('\n\tEntrada\t\tSalida\n')
+for entrada in [[1, 0, 1], [1, 1, 1], [0, 1, 0, 0, 0], [1, 0, 1, 1, 0, 1], [0], [1]]:
+    print(f'\t{corregir(entrada)}\t\t{corregir(paridad_turing(entrada))}')
