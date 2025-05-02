561220ffad87503122928e0201963847a7b2e7f8
Martina Pauer <martinapauer38@gmail.com>
2025-05-02 22:59:48 +0000

    Completando pero debo hacer correciones El lunes sigo desarrollando anda de a-d y d-b pero no anda para a-c siendo ligeramente mas largo el recorrido.

--- dijkstra/Python/dij.py
+++ dijkstra/Python/dij.py
@@ -18,9 +18,9 @@ def representar(grafos : list):
 
 primera_parte = grafo(97, 1, 98)
 
-segunda_parte = grafo(98, 1, 99)
+segunda_parte = grafo(98, 3, 99)
 # Si lleva solo a un vertice el peso es 1 sino por cada nuevo vertice sube 1
-tercera_parte = grafo(99, primera_parte.ponderado + 1, 100)
+tercera_parte = grafo(100, primera_parte.ponderado + 1, 99)
 
 cuarta_parte = grafo(100, 1, 102)
 
@@ -33,3 +33,15 @@ print(f'El grafo es: {representar(partes)}\n')
 inicio = input('\tIngrese caracter de inicio: ')
 
 fin = input('\tIngrese caracter a donde ir: ')
+
+recorrido = []
+
+for recorrer in partes:
+   if ((chr(recorrer.vertice_origen) == inicio) or (chr(recorrer.vertice_destino) == inicio) or (chr(recorrer.vertice_origen) == fin) or (chr(recorrer.vertice_destino) == fin)):
+      if recorrer.ponderado < 2:
+         continue
+      else:
+          recorrido.append(recorrer)
+
+
+print(f'\nEl recorrido de Dijkstra encontrado es {representar(recorrido).__str__()}')   
